fc286ab6f62f03a0a6aa9f669678aef6985842ce
Ethan Chan <metakirby5@gmail.com>
2015-12-28 00:51:15 -0800

    add packaging stuff

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='whizkers',
+    version='1.0.0',
+    description='Mustache + YAML based config templater.',
+    long_description=long_description,
+    url='https://github.com/metakirby5/whizkers',
+    author='Ethan Chan',
+    author_email='metakirby5@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='whizkers config templater mustache yaml',
+    packages=find_packages(),
+    install_requires=[
+        'argcomplete',
+        'colorlog',
+        'pystache',
+        'PyYAML',
+        'termcolor',
+        'watchdog',
+    ],
+    entry_points={
+        'console_scripts': [
+            'whizkers=whizkers:main',
+        ],
+    },
+)
+
