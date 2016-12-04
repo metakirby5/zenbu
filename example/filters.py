7b2d1bd9ef0771d00b02482e57531e976a348efd
Ethan Chan <metakirby5@gmail.com>
2016-12-03 16:02:25 -0800

    fix to_rgb, add to_chrome and to_apple

--- example/filters.py
+++ example/filters.py
@@ -1,4 +1,5 @@
 import struct
+import codecs
 
 # Hex to X
 
@@ -6,4 +7,13 @@ def nohash(hex):
   return hex.lstrip('#')
 
 def to_rgb(hex):
-  return struct.unpack('BBB', nohash(hex).decode('hex'))
+  return struct.unpack('BBB', codecs.decode(nohash(hex), 'hex'))
+
+def to_chrome(hex):
+  return '[{}]'.format(', '.join(map(str, to_rgb(hex))))
+
+# alpha: 0 - 255
+def to_apple(hex, alpha=0):
+  return '{{{}}}'.format(', '.join(map(
+    lambda x: str(x * 257), to_rgb(hex) + (alpha,)
+  )))
